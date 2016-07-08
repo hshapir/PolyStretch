77fe258a9124d2039173f660c1cb29db95b51802
Harrison Shapiro <Harrison Shapiro@10.150.3.10>
2016-07-08 11:59:17 -0500

    Added some initial handlers for button actions

--- src/polystretch/Control.java
+++ src/polystretch/Control.java
@@ -11,10 +11,13 @@ package polystretch;
  */
 public class Control {
     public long distance;
+    public static int totalStepCount;
     
-    public static void stepMotor(){
-        
-        
-        
+    public static void stepMotor(int numSteps){
+        if(numSteps > 0){
+            for(int i = 0; i < numSteps; i++){
+                Control.totalStepCount++;
+            }
+        }
     }
 }
